8855dbab1e19de80338ffa09fbaa172c5dc37f50
Matthew Man <43723450+Matthew-Man@users.noreply.github.com>
2021-08-31 23:26:41 +0100

    Create MessageSelection.tsx

---
+++ src/MessageSelection.tsx
@@ -0,0 +1,24 @@
+import { AddIcon, SearchIcon } from "@chakra-ui/icons";
+import { Box, Flex, Heading, HStack, Icon, Spacer } from "@chakra-ui/react";
+
+export default function MessageSelection() {
+  return (
+    <Box
+      bgColor="white"
+      h="90vh"
+      p="40px"
+      borderRight={1}
+      borderStyle="solid"
+      borderColor="gray.200"
+    >
+      <Flex>
+        <Heading size="md" fontWeight="medium">Chats</Heading>
+        <Spacer />
+        <HStack spacing="20px">
+          <Icon as={SearchIcon} />
+          <Icon as={AddIcon} />
+        </HStack>
+      </Flex>
+    </Box>
+  )
+}
