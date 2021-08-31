c4cb3e96e4d133d33d3aba15bde5649e6c045f6a
Matthew Man <43723450+Matthew-Man@users.noreply.github.com>
2021-09-01 00:35:19 +0100

    New stevechat logo

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, HStack, Icon, IconButton, Link, SkeletonCircle, Spacer, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, HStack, Icon, Image, IconButton, Link, SkeletonCircle, Spacer, Text } from "@chakra-ui/react";
 import { AiFillMessage } from "react-icons/ai";
 import { GrNotification } from "react-icons/gr"
 
@@ -7,8 +7,8 @@ export default function Header() {
     <Box p="0px 20px" borderBottom={1} borderStyle="solid" borderColor="gray.200" bgColor="white">
       <Flex h="10vh" align="center" >
         <Link href="/">
-          <HStack>
-            <Icon as={AiFillMessage} w={10} h={10} color="blue.300" />
+          <HStack spacing="0px">
+            <Image src="/stevechat-steve.png" alt="Stevechat Steve Chick" h="60px" w="60px" />
             <Heading fontSize="24px" fontWeight="medium" color="blue.800">SteveChat Web</Heading>
           </HStack>
         </Link>
