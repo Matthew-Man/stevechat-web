a7fd7f251b00f995c5e71374ec20e58adfb18552
Matthew Man <43723450+Matthew-Man@users.noreply.github.com>
2021-09-23 07:34:48 +0100

    Added placeholder text and adjusted w

--- src/MessageBubble.tsx
+++ src/MessageBubble.tsx
@@ -4,12 +4,11 @@ interface MessageBubbleProps {
   isMyMessage?: boolean;
 }
 
-export default function MessageBubble({ isMyMessage = false }: MessageBubbleProps) {
+export default function MessageBubble({
+  isMyMessage = false,
+}: MessageBubbleProps) {
   return (
-    <Box
-      maxWidth="60%"
-      m="10px 0px"
-      float={isMyMessage ? "right" : "left"}>
+    <Box maxWidth="70%" m="10px 0px" float={isMyMessage ? "right" : "left"}>
       <HStack spacing="10px" align="flex-start">
         {!isMyMessage && <MessageBubbleProfileImage />}
         <Box
@@ -17,13 +16,19 @@ export default function MessageBubble({ isMyMessage = false }: MessageBubbleProp
           bgColor={isMyMessage ? "blue.500" : "gray.100"}
           color={isMyMessage ? "white" : "gray.700"}
           width="90%"
-          borderRadius="15px">
-          <Text textAlign={isMyMessage ? "right" : "left"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text>
+          borderRadius="15px"
+        >
+          <Text textAlign={isMyMessage ? "right" : "left"}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </Text>
         </Box>
         {isMyMessage && <MessageBubbleProfileImage />}
       </HStack>
     </Box>
-  )
+  );
 }
 
 function MessageBubbleProfileImage() {
@@ -37,5 +42,5 @@ function MessageBubbleProfileImage() {
       src="https://images.unsplash.com/photo-1520341280432-4749d4d7bcf9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2734&q=80"
       alt="User photo"
     />
-  )
+  );
 }
