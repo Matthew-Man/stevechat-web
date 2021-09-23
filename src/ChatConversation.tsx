0708633dada2c5994ac1779f3f6cd94a00e54334
Matthew Man <43723450+Matthew-Man@users.noreply.github.com>
2021-09-23 07:36:17 +0100

    Formatting

--- src/ChatConversation.tsx
+++ src/ChatConversation.tsx
@@ -1,21 +1,38 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import MessageBubble from "./MessageBubble";
+import MessageInputs from "./MessageInputs";
 
 export default function ChatConversation() {
   return (
-    <Box bgColor="white" p="40px">
-      <Heading fontSize="24px" color="gray.700">Timothy Placename</Heading>
-      <Box bgColor="blue.50" maxW="60%" p="20px 40px" m="40px 40px 40px -40px" bgGradient="linear(to-r, #e3f0fa, #f2f9ff)">
-        <Heading
-          size="xs"
-          color="blue.400"
-        >Welcome to your personal Stevechat conversation</Heading>
-        <Text mt="6px" fontSize="14px" color="#6e8a9c" fontWeight="med">Conversation description placeholder. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text>
+    <>
+      <Box bgColor="white" p="40px">
+        <Heading fontSize="24px" color="gray.700">
+          Timothy Placename
+        </Heading>
+        <Box
+          bgColor="blue.50"
+          maxW={{ base: "80%", m: "60%" }}
+          p="20px 40px"
+          m="40px 40px 40px -40px"
+          bgGradient="linear(to-r, #e3f0fa, #f2f9ff)"
+        >
+          <Heading size="xs" color="blue.400">
+            Welcome to your personal Stevechat conversation
+          </Heading>
+          <Text mt="6px" fontSize="14px" color="#6e8a9c" fontWeight="med">
+            Conversation description placeholder. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.{" "}
+          </Text>
+        </Box>
+        <Box mt="40px">
+          <MessageBubble />
+          <MessageBubble isMyMessage />
+        </Box>
       </Box>
-      <Box mt="40px">
-        <MessageBubble />
-        <MessageBubble isMyMessage />
+      <Box>
+        <MessageInputs />
       </Box>
-    </Box>
-  )
+    </>
+  );
 }
